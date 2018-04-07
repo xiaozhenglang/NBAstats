2d1a2802b3c0235d8ebec222e889d53afc3b7c2b
xiaozhenglang <xiaozheng.lang1@gmail.com>
2018-04-07 16:59:18 -0600

    shotchart

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,24 @@
 import React from "react";
 import {ShotChart} from "./ShotChart"
-
+import nba from "nba";
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
+    }
+
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playerInfo);
+            this.setState({playerInfo});
+        });
+    }
+
     render() {
         return(
             <div>
-               <ShotChart playerId={2544}/>
+               <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }
